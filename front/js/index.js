0ab3b864eeb3fafb9566b7d3ba5ff977ded72aeb
LorisBST <bouissetloris@gmail.com>
2022-05-13 18:46:44 +0200

    index.html
    Index HTML terminé

---
+++ front/js/index.js
@@ -0,0 +1,48 @@
+fetch("http://localhost:3000/api/products")
+.then(function(reponse){
+return reponse.json();
+})
+.then(function(reponseFormate){
+let affichage = "";
+// 
+for(var produit of reponseFormate){
+    affichage += `<a href="./product.html?id=${produit._id}">
+    <article>
+     <img src="${produit.imageUrl}" alt="${produit.altTxt}">     
+     <h3 class="productName">${produit.name}</h3>
+     <p class="productDescription">${produit.description}</p>
+    </article>
+   </a>\n`; 
+}
+document.getElementById("items").innerHTML = affichage;
+
+})
+
+.catch(function (erreur){
+    alert("Serveur inaccessible.");
+} )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  for(var i = 0; i < reponseFormate.length; i++) {
+//      console.log(reponseFormate[i]);
+//  }
+
+    // var i = 0;
+    // while(i < reponseFormate.length) {
+    //     console.log(reponseFormate[i]);
+    //     i++;
+    // }
+
+
